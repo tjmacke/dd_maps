a2b476a45eea44443015777d2e38aa926f8fc4c0
Tom Macke <macke_tom@yahoo.com>
2016-03-25 13:53:32 -0700

    *.sh		-- added code to properly process the args

--- scripts/mk_pay_summary.sh
+++ scripts/mk_pay_summary.sh
@@ -2,7 +2,34 @@
 #
 . ~/etc/funcs.sh
 
-FILE=$1
+U_MSG="usage: $0 [ -help ] [ payments-flle ]"
+
+FILE=
+
+while [ $# -gt 0 ] ; do
+	case $1 in
+	-help)
+		echo "$U_MSG"
+		exit 0
+		;;
+	-*)
+		LOG ERROR "unknown option $1"
+		echo "$U_MSG" 1>&2
+		exit 1
+		;;
+	*)
+		FILE=$1
+		shift
+		break
+		;;
+	esac
+done
+
+if [ $# -ne 0 ] ; then
+	LOG ERROR "extra arguments $*"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
 
 awk -F'\t'	'BEGIN {
 	pr_hdr = 1
