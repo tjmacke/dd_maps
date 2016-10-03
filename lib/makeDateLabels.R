7df985b15427d0eb1cc422caebeaf2fa2b9fbf43
Tom Macke <macke@kismet.tr.zoo>
2016-10-03 11:42:04 -0700

    Went back to strtoi() after adding base args (, base=10)

--- lib/makeDateLabels.R
+++ lib/makeDateLabels.R
@@ -1,11 +1,8 @@
 makeDateLabels <- function(dv) {
 	u <- paste(unique(substr(as.character(dv), 1, 7)), '01', sep='-')
 
-	y_last <- as.integer(substr(u[length(u)], 1, 4))
-	m_last <- as.integer(substr(u[length(u)], 6, 7))
-
-#	y_last
-#	m_last
+	y_last <- strtoi(substr(u[length(u)], 1, 4), base=10)
+	m_last <- strtoi(substr(u[length(u)], 6, 7), base=10)
 
 	if (m_last == 12) {
 		m_end = 1
