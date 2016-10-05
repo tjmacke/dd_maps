31edfb2ce19ed9f9227756733ff7ef89a4c81744
Tom Macke <macke_tom@yahoo.com>
2016-10-05 12:05:55 -0700

    Added code to get the model slopes and add Up, Flat or Down to the legend for >0, =0, <0

--- lib/plotPayRates.R
+++ lib/plotPayRates.R
@@ -7,8 +7,14 @@ plotPayRates <- function(df, pfn='') {
 	source(sfn, chdir=T)
 
 	m_hrate <- lm(df$hrate ~ as.Date(df$date, '%Y-%m-%d'))
+	slope <- summary(m_hrate)$coefficients[2,1]
+	d_hrate <- ifelse(slope > 0, 'Up', ifelse(slope == 0, 'Flat', 'Down'))
 	m_drate <- lm(df$drate ~ as.Date(df$date, '%Y-%m-%d'))
+	slope <- summary(m_drate)$coefficients[2,1]
+	d_drate <- ifelse(slope > 0, 'Up', ifelse(slope == 0, 'Flat', 'Down'))
 	m_dph <- lm(df$dph ~ as.Date(df$date, '%Y-%m-%d'))
+	slope <- summary(m_dph)$coefficients[2,1]
+	d_dph <- ifelse(slope > 0, 'Up', ifelse(slope == 0, 'Flat', 'Down'))
 
 	if (pfn != '') {
 		pdf(file=pfn)
@@ -22,7 +28,9 @@ plotPayRates <- function(df, pfn='') {
 		type='n',
 		xlab='Date',
 		xaxt='n',
+		yaxt='n',
 		ylab='Rates ($/hr)')
+	axis(side=2, at=seq(from=0, to=50, by=5), labels=T, las=1)
 	grid(col='black')
 	lines(as.Date(df$date, '%Y-%m-%d'), df$hrate, col='red')
 	abline(m_hrate, col='red', lty=2)
@@ -30,9 +38,12 @@ plotPayRates <- function(df, pfn='') {
 	abline(m_drate, col='green', lty=2)
 	lines(as.Date(df$date, '%Y-%m-%d'), df$dph, col='blue')
 	abline(m_dph, col='blue', lty=2)
-	title('Doordash Rates')
+	l_date <- df[length(df$date), 1]
+	title(paste('Doordash Rates', l_date, sep=' through '))
 	axis(1, at=dl$tk, labels=dl$lb)
-	legend('topleft', inset=c(0.05, 0.05), bg='gray96', legend=c('$/Hour', '$/Dash', 'Dashes/Hour'), col=c('red', 'green', 'blue'), lty=1)
+	legend('topleft', inset=c(0.05, 0.05), bg='gray96',
+		legend=c(paste('$/Hour', d_hrate, sep=', '), paste('$/Dash', d_drate, sep=', '), paste('Dashes/Hour', d_dph, sep=', ')),
+		col=c('red', 'green', 'blue'), lty=1)
 	if (pfn != '') {
 		ign <- dev.off()
 	}
