a91eafcbc35a65198bd4a923006fd19652221842
Tom Macke <macke_tom@yahoo.com>
2016-10-27 10:23:07 -0700

    map_by_date.sh		-- works from runs, deals w/src or dst addr and allows for native or reversed w/unit

--- scripts/map_by_date.sh
+++ scripts/map_by_date.sh
@@ -1,14 +1,25 @@
 #! /bin/bash
 #
 . ~/etc/funcs.sh
-
 export LC_ALL=C
 
-U_MSG="usage: $0 [ -help ] -a addr-file [ -cnt ] [ dates-file ]"
+U_MSG="usage: $0 [ -help ] -a addr-file -at { src | dst } [ -cnt ] [ -rev ] [ -unit { day | week | month } ] [ runs-file ]"
+
+if [ -z "$DM_HOME" ] ; then
+	LOG ERROR "DM_HOME is not defined"
+	exit 1
+fi
+
+DM_ETC=$DM_HOME/etc
+DM_LIB=$DM_HOME/lib
+DM_SCRIPTS=$DM_HOME/scripts
 
 AFILE=
+ATYPE==
 CNT=
-FILE=$1
+REV=
+UNIT=
+FILE=
 
 while [ $# -gt 0 ] ; do
 	case $1 in
@@ -26,8 +37,32 @@ while [ $# -gt 0 ] ; do
 		AFILE=$1
 		shift
 		;;
+	-at)
+		shift
+		if [ $# -eq 0 ] ; then
+			LOG ERROR "-at requires address-type argument"
+			echo "$U_MSG" 1>&2
+			exit 1
+		fi
+		ATYPE=$1
+		shift
+		;;
 	-cnt)
 		CNT="yes"
+		shift
+		;;
+	-rev)
+		REV="yes"
+		shift
+		;;
+	-unit)
+		shift
+		if [ $# -eq 0 ] ; then
+			LOG ERROR "-unit requires unit argument"
+			echo "$U_MSG" 1>&2
+			exit 1
+		fi
+		UNIT=$1
 		shift
 		;;
 	-*)
@@ -55,7 +90,32 @@ if [ -z "$AFILE" ] ; then
 	exit 1
 fi
 
-sort -t $'\t' -k 2,2 -k 1,1 $FILE	|\
+if [ -z "$ATYPE" ] ; then
+	LOG ERROR "missing -at address-type argument"
+	echo "$U_MSG" 1>&2
+	exit 1
+elif [ "$ATYPE" != "src" ] && [ "$ATYPE" != "dst" ] ; then
+	LOG ERROR "unknown address type $ATYPE, must be src or dst"
+	echo "$U_MSG" 1>&2
+	exit 1
+fi
+
+if [ ! -z "$UNIT" ] ; then
+	if [ "$UNIT" != "day" ] && [ "$UNIT" != "week" ] && [ "$UNIT" != "month" ] ; then
+		LOG ERROR "unknown unit $UNIT, must be day, week, month"
+		echo "$U_MSG" 1>&2
+		exit 1
+	fi
+fi
+
+awk -F'\t' 'BEGIN {
+	atype = "'"$ATYPE"'"
+	f_atype = atype == "src" ? 6 : 7
+}
+$5 == "Job" {
+	printf("%s\t%s\n", $1, $f_atype)
+}' $FILE	|\
+sort -t $'\t' -k 2,2 -k 1,1	|\
 awk -F'\t' 'BEGIN {
 	afile = "'"$AFILE"'"
 	for(n_a2idx = 0; (getline < afile) > 0; ){
@@ -88,19 +148,64 @@ END {
 		l_2 = ""
 		cnt = 0
 	}
-}' |\
+}'		|\
 awk -F'\t' 'BEGIN {
 	cnt = "'"$CNT"'" == "yes"
+	rev = "'"$REV"'" == "yes"
+	unit = "'"$UNIT"'"
+	if(unit == "")
+		unit = 1
+	else if(unit == "day")
+		unit = 86400
+	else if(unit == "week")
+		unit = 7 * 86400
+	else if(unit == "month")
+		unit = 30 * 86400
+	else{
+		printf("ERROR: unknown unit: %s\n", unit) > "/dev/stderr"
+		err = 1
+		exit err
+	}
 }
 {
-	work = $1
-	gsub(/-/, " ", work)
-	t = mktime(work " 00 00 00")
-	if(cnt){
-		label = sprintf("visits=%d, last=%s", $2, $1)
-		printf("%d\t%s\t%s\t%s\t%s\t%s\n", t, $2, label, $3, $4, $5)
-	}else{
-		label = sprintf("last=%s", $1)
-		printf("%d\t%s\t%s\t%s\t%s\n", t, label, $3, $4, $5)
+	n_addrs++
+	dates[n_addrs] = $1
+	cnts[n_addrs] = $2
+	addrs[n_addrs] = $3
+	lngs[n_addrs] = $4
+	lats[n_addrs] = $5
+}
+END {
+	if(err)
+		exit err
+
+	if(rev){
+		date_max = dates[1]
+		for(i = 2; i <= n_addrs; i++){
+			if(dates[i] > date_max)
+				date_max = dates[i]
+		}
+		gsub(/-/, " ", date_max)
+		t_max = mktime(date_max " 00 00 00")
+	}
+
+	for(i = 1; i <= n_addrs; i++){
+		work = dates[i]
+		gsub(/-/, " ", work)
+		t = mktime(work " 00 00 00")
+		t = rev ? t_max - t : t
+		t /= unit
+		if(unit == 1)
+			printf("%d", t)
+		else
+			printf("%g", t)
+		if(cnt){
+			label = sprintf("visits=%d, last=%s", cnts[i], dates[i])
+			printf("\t%d\t%s", cnts[i], label)
+		}else{
+			label = sprintf("last=%s", dates[i])
+			printf("\t%s", label)
+		}
+		printf("\t%s\t%s\t%s\n", addrs[i], lngs[i], lats[i])
 	}
 }'
