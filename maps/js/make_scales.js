825a30d5a310dec978abe9fe87312d36759f949b
Tom Macke <macke_tom@yahoo.com>
2016-11-10 13:24:17 -0800

    make_scales.js		-- refactored into 2 scales each with data + config

--- maps/js/make_scales.js
+++ maps/js/make_scales.js
@@ -1,37 +1,49 @@
-function makeScales(config) {
+function makeScales(scaleData) {
 
-  // get everything parameterized.
-  // color values
-  var cv_tick_font_size = 12;
-  var cv_xoff = 5;
-  var cv_width = 40;
-  var cv_height = 15;
-  var cv_tick_len = 5
-  var cv_xHeight = Math.round(0.7 * cv_tick_font_size);
-  var cv_yoff = cv_tick_font_size + 2;
-  var cv_tick_yoff = cv_yoff + cv_height
-  var cv_tick_text_yoff = cv_tick_yoff + cv_tick_len + cv_xHeight;
+  var colorScaleConfig = {}
+  colorScaleConfig.sc_tick_font_size = 12;
+  colorScaleConfig.sc_xoff           = 5;
+  colorScaleConfig.sc_width          = 40;
+  colorScaleConfig.sc_height         = 15;
+  colorScaleConfig.sc_tick_len       = 5;
+  colorScaleConfig.sc_xHeight        = Math.round(0.7 * colorScaleConfig.sc_tick_font_size);
+  colorScaleConfig.sc_yoff           = colorScaleConfig.sc_tick_font_size + 2;
+  colorScaleConfig.sc_tick_yoff      = colorScaleConfig.sc_yoff + colorScaleConfig.sc_height;
+  colorScaleConfig.sc_tick_text_yoff = colorScaleConfig.sc_tick_yoff + colorScaleConfig.sc_tick_len + colorScaleConfig.sc_xHeight;
+  makeColorScale(scaleData, colorScaleConfig);
 
-  // TODO: check that the scale(s) can be made
+  var v2ScaleConfig = {};
+  v2ScaleConfig.sc_tick_font_size = 12;
+  v2ScaleConfig.sc_xoff           = 5;
+  v2ScaleConfig.sc_width          = 70;
+  v2ScaleConfig.sc_height         = 15;
+  v2ScaleConfig.sc_tick_len       = 5;
+  v2ScaleConfig.sc_xHeight        = Math.round(0.7 * v2ScaleConfig.sc_tick_font_size);
+  v2ScaleConfig.sc_yoff           = v2ScaleConfig.sc_tick_font_size + 2;
+  v2ScaleConfig.sc_tick_yoff      = v2ScaleConfig.sc_yoff + v2ScaleConfig.sc_height;
+  v2ScaleConfig.sc_tick_text_yoff = v2ScaleConfig.sc_tick_yoff + v2ScaleConfig.sc_tick_len + v2ScaleConfig.sc_xHeight;
+  makeV2Scale(scaleData, v2ScaleConfig);
+}
 
-  // BEGIN Pin color scale
+function makeColorScale(scaleData, scaleConfig) {
+
   // Add the svg 
   d3.select("#colorScale")
     .append("svg")
     .attr("id", "svgColorScale")
     .attr("height", 50)
-    .attr("width", config.color_values.length * cv_width + 2 * cv_xoff);
+    .attr("width", scaleData.color_values.length * scaleConfig.sc_width + 2 * scaleConfig.sc_xoff);
 
   // Draw the colored boxes
   d3.select("#svgColorScale")
     .selectAll("rect")
-    .data(config.color_values)
+    .data(scaleData.color_values)
     .enter()
     .append("rect")
-    .attr("width", cv_width)
-    .attr("height", cv_height)
-    .attr("x", function(d, i) { return i * cv_width + cv_xoff; })
-    .attr("y", cv_yoff)
+    .attr("width", scaleConfig.sc_width)
+    .attr("height", scaleConfig.sc_height)
+    .attr("x", function(d, i) { return i * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y", scaleConfig.sc_yoff)
     .style("fill", function(d) {
       return "rgb(" + Math.round(d[0]*255) + "," + Math.round(d[1]*255) + "," + Math.round(d[2]*255) + ")";
     })
@@ -41,72 +53,62 @@ function makeScales(config) {
   // Draw the ticks
   d3.select("#svgColorScale")
     .selectAll("line")
-    .data(config.color_breaks)
+    .data(scaleData.color_breaks)
     .enter()
     .append("line")
-    .attr("x1", function(d, i) { return (i+1) * cv_width + cv_xoff; })
-    .attr("y1", cv_tick_yoff)
-    .attr("x2", function(d, i) { return (i+1) * cv_width + cv_xoff; })
-    .attr("y2", cv_tick_yoff + cv_tick_len)
+    .attr("x1", function(d, i) { return (i+1) * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y1", scaleConfig.sc_tick_yoff)
+    .attr("x2", function(d, i) { return (i+1) * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y2", scaleConfig.sc_tick_yoff + scaleConfig.sc_tick_len)
     .style("stroke", "black")
     .style("stroke-width", "1px");
 
   // Draw the tick labels
   d3.select("#svgColorScale")
     .selectAll("text.tick")
-    .data(config.color_breaks)
+    .data(scaleData.color_breaks)
     .enter()
     .append("text")
     .attr("class", "tick")
-    .attr("x", function(d, i) { return (i+1) * cv_width + cv_xoff; } )
-    .attr("y", cv_tick_text_yoff)
-    .attr("font-size", cv_tick_font_size)
+    .attr("x", function(d, i) { return (i+1) * scaleConfig.sc_width + scaleConfig.sc_xoff; } )
+    .attr("y", scaleConfig.sc_tick_text_yoff)
+    .attr("font-size", scaleConfig.sc_tick_font_size)
     .attr("text-anchor", "middle")
     .text(function(d) { return d; });
 
   // Draw the scale title
-  var color_title = ("color_title" in config) ? config.color_title : ["Pin Colors"];
+  var color_title = ("color_title" in scaleData) ? scaleData.color_title : ["Pin Colors"];
   d3.select("#svgColorScale")
     .selectAll("text.title")
     .data(color_title)
     .enter()
     .append("text")
     .attr("class", "title")
-    .attr("x", 0.5 * (config.color_values.length * cv_width) + cv_xoff)
-    .attr("y", cv_xHeight + 2)
-    .attr("font-size", cv_tick_font_size)
+    .attr("x", 0.5 * (scaleData.color_values.length * scaleConfig.sc_width) + scaleConfig.sc_xoff)
+    .attr("y", scaleConfig.sc_xHeight + 2)
+    .attr("font-size", scaleConfig.sc_tick_font_size)
     .attr("text-anchor", "middle")
     .text(function(d) { return d; });
-  // END Pin color scale
+}
 
-  // TODO: merge this with cv_*
-  var cb_tick_font_size = 12;
-  var cb_xoff = 5;
-  var cb_width = 70;
-  var cb_height = 15;
-  var cb_tick_len = 5;
-  var cb_xHeight = Math.round(0.7 * cb_tick_font_size);
-  var cb_yoff = cb_tick_font_size + 2;
-  var cb_tick_yoff = cb_yoff + cb_height;
-  var cb_tick_text_yoff = cb_tick_yoff + cb_tick_len + cb_xHeight;
+function makeV2Scale(scaleData, scaleConfig) {
 
-  // BEGIN Pin size scale
   d3.select("#v2Scale")
     .append("svg")
     .attr("id", "svgV2Scale")
     .attr("height", 50)
-    .attr("width", config.v2_values.length * cb_width + 2 * cb_xoff);
+    .attr("width", scaleData.v2_values.length * scaleConfig.sc_width + 2 * scaleConfig.sc_xoff);
 
   // Draw white boxes
   d3.select("#svgV2Scale")
     .selectAll("rect")
-    .data(config.v2_values)
+    .data(scaleData.v2_values)
     .enter()
     .append("rect")
-    .attr("width", cb_width)
-    .attr("height", cb_height)
-    .attr("x", function(d, i) { return i * cb_width + cb_xoff; })
-    .attr("y", cb_yoff)
+    .attr("width", scaleConfig.sc_width)
+    .attr("height", scaleConfig.sc_height)
+    .attr("x", function(d, i) { return i * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y", scaleConfig.sc_yoff)
     .style("fill", "white")
     .style("stroke", "black")
     .style("stroke-width", "1px");
@@ -114,55 +116,53 @@ function makeScales(config) {
   // Put the size values in the boxes
   d3.select("#svgV2Scale")
     .selectAll("text.label")
-    .data(config.v2_values)
+    .data(scaleData.v2_values)
     .enter()
     .append("text")
     .attr("class", "label")
-    .attr("x", function(d, i) { return (i + 0.5) * cb_width + cb_xoff; })
-    .attr("y", cb_tick_yoff - 4)
-    .attr("font-size", cb_tick_font_size)
+    .attr("x", function(d, i) { return (i + 0.5) * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y", scaleConfig.sc_tick_yoff - 4)
+    .attr("font-size", scaleConfig.sc_tick_font_size)
     .attr("text-anchor", "middle")
     .text(function(d) { return d; });
 
   // Draw the ticks
   d3.select("#svgV2Scale")
     .selectAll("line")
-    .data(config.v2_breaks)
+    .data(scaleData.v2_breaks)
     .enter()
     .append("line")
-    .attr("x1", function(d, i) { return (i+1) * cb_width + cb_xoff; })
-    .attr("y1", cb_tick_yoff)
-    .attr("x2", function(d, i) { return (i+1) * cb_width + cb_xoff; })
-    .attr("y2", cb_tick_yoff + cb_tick_len)
+    .attr("x1", function(d, i) { return (i+1) * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y1", scaleConfig.sc_tick_yoff)
+    .attr("x2", function(d, i) { return (i+1) * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y2", scaleConfig.sc_tick_yoff + scaleConfig.sc_tick_len)
     .style("stroke", "black")
     .style("stroke-width", "1px");
 
   // Draw the tick labels
   d3.select("#svgV2Scale")
     .selectAll("text.tick")
-    .data(config.v2_breaks)
+    .data(scaleData.v2_breaks)
     .enter()
     .append("text")
     .attr("class", "tick")
-    .attr("x", function(d, i) { return (i+1) * cb_width + cb_xoff; })
-    .attr("y", cb_tick_text_yoff)
-    .attr("font-size", cb_tick_font_size)
+    .attr("x", function(d, i) { return (i+1) * scaleConfig.sc_width + scaleConfig.sc_xoff; })
+    .attr("y", scaleConfig.sc_tick_text_yoff)
+    .attr("font-size", scaleConfig.sc_tick_font_size)
     .attr("text-anchor", "middle")
     .text(function(d) { return d; });
 
-  var v2_title = ("v2_title" in config) ? config.v2_title : ["Pin Sizes"];
+  // Draw the scale title
+  var v2_title = ("v2_title" in scaleData) ? scaleData.v2_title : ["Pin Sizes"];
   d3.select("#svgV2Scale")
     .selectAll("text.title")
     .data(v2_title)
     .enter()
     .append("text")
     .attr("class", "title")
-    .attr("x", 0.5 * (config.v2_values.length * cb_width) + cb_xoff)
-    .attr("y", cb_xHeight + 2)
-    .attr("font-size", cb_tick_font_size)
+    .attr("x", 0.5 * (scaleData.v2_values.length * scaleConfig.sc_width) + scaleConfig.sc_xoff)
+    .attr("y", scaleConfig.sc_xHeight + 2)
+    .attr("font-size", scaleConfig.sc_tick_font_size)
     .attr("text-anchor", "middle")
     .text(function(d) { return d; });
-
-  // END Pin size scale
-
 }
