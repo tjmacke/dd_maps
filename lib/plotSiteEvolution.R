8a7180cadc4135a1fa689d26ce7a59c9c6d4a11f
Tom Macke <macke_tom@yahoo.com>
2016-12-13 18:10:09 -0800

    *.R, *.sh	-- R and *.sh driver that plots the "evolution", ie time since last visit

---
+++ lib/plotSiteEvolution.R
@@ -0,0 +1,53 @@
+plotSiteEvolution <- function(df, stype, pfn='') {
+
+	if (!exists('dm_home')) {
+		stop('dm_home is not defined.', call.=T)
+	}
+	sfn <- paste(dm_home, 'lib', 'makeDateLabels.R', sep='/')
+	source(sfn, chdir=T)
+
+	if (pfn != '') {
+		pdf(file=pfn)
+	} else {
+		X11()
+	}
+	dl <- makeDateLabels(df$date)
+
+	# TODO: replace c(0, 60) with a properly scaled Y-Axis
+	plot(
+		c(as.Date(dl$tk[1], '%Y-%m-%d'), as.Date(dl$tk[length(dl$tk)], '%Y-%m-%d')),
+		c(0, 60),
+		type='n',
+		xlab='Month',
+		xaxt='n',
+		yaxt='n',
+		ylab='Percent of something')
+
+	axis(1, at=dl$tk, label=F)
+	y_adj <- 4.2
+	text(dl$tk, y = 0 - y_adj, label=dl$lb, srt=45, pos=2, off=-0.2, xpd=T, cex=0.8)
+	axis(2, at=seq(from=0, to=100, by=10), labels=T, las=1)
+
+	# draw a nice grid
+	abline(h=seq(from=0, to=100, by=10), lty=3, col='black')
+	abline(v=dl$tk, lty=3, col='black')
+
+	# TODO: require > 1 point?
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le1, lwd=1.5, col='red')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le2, lwd=1.5, col='orange')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le4, lwd=1.5, col='yellow')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le8, lwd=1.5, col='green')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le12, lwd=1.5, col='cyan')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le26, lwd=1.5, col='lightblue')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$le52, lwd=1.5, col='gray')
+	lines(as.Date(df$date, '%Y-%m-%d'), df$gt52, lwd=1.5, col='black')
+
+	lgnd = c('<= 1 week', '<= 2 weeks', '<= 4 weeks', '<= 8 weeks', '<= 12 weeks', '<= 26 weeks', '<= 52 weeks', '> 52 weeks')
+	legend('topleft', inset=c(0.08, 0.02), bg='white',
+		legend=lgnd,
+		col=c('red', 'orange', 'yellow', 'green', 'cyan', 'lightblue', 'gray', 'black'), lty=1, cex=0.7)
+
+	if (pfn != '') {
+		ign <- dev.off()
+	}
+}
---
+++ lib/plotSiteEvolutionMain.R
@@ -0,0 +1,46 @@
+dm_home <- Sys.getenv(c('DM_HOME'))
+if (dm_home == '') {
+	stop('DM_HOME is not defined.', call.=F)
+}
+sfn <- paste(dm_home, 'lib', 'plotSiteEvolution.R', sep='/')
+source(sfn, chdir=T)
+
+args <- commandArgs(trailingOnly=T)
+
+stype <- ''
+dfn <- ''
+
+i = 1
+while(i <= length(args)) {
+	if (args[i] == "-at") {
+		i <- i + 1
+		if (i == length(args)) {
+			stop('ERROR: -at requires address-type argument', call.=F)
+		}
+		stype <- args[i]
+	} else if(substring(args[i], 1, 1) == '-') {
+		stop(paste('ERROR: unknown option', args[i], sep=': '))
+	} else {
+		dfn <- args[i]
+		break
+	}
+	i <- i + 1
+}
+
+if (i < length(args)) {
+	stop('ERROR: extra arguments')
+}
+if (stype == '') {
+	stop('ERROR: missing -at { src | dst } argument')
+} else if(stype != 'src' && stype != 'dst') {
+	stop(paste('ERROR: unknown address type', stype, 'must be src or dst', sep=' '))
+}
+
+if (dfn == '') {
+	stop('ERROR: missing site-evolution-file')
+}
+
+df <- read.csv(dfn, sep='\t')
+ofn = sprintf('%sEvolution.%s.pdf', stype, format(Sys.time(), '%Y-%m-%d'))
+
+plotSiteEvolution(df, stype, ofn)
