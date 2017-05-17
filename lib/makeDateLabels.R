9c4fa849cd1cc59af058d201f2c18a467b051e86
Tom Macke <macke_tom@yahoo.com>
2017-05-17 10:52:44 -0700

    makeDateLabels.R	-- Added posix=T flag as as.Date() and as.POSIXct() can not be graphed together

--- lib/makeDateLabels.R
+++ lib/makeDateLabels.R
@@ -1,9 +1,8 @@
-makeDateLabels <- function(dv) {
-	u <- paste(unique(substr(as.character(dv), 1, 7)), '01', sep='-')
+makeDateLabels <- function(dv, posix=F) {
 
-	y_last <- strtoi(substr(u[length(u)], 1, 4), base=10)
-	m_last <- strtoi(substr(u[length(u)], 6, 7), base=10)
-
+	u_dv <- paste(unique(substr(as.character(dv), 1, 7)), '01', sep='-')
+	y_last <- strtoi(substr(u_dv[length(u_dv)], 1, 4), base=10)
+	m_last <- strtoi(substr(u_dv[length(u_dv)], 6, 7), base=10)
 	if (m_last == 12) {
 		m_end = 1
 		y_end = y_last + 1
@@ -11,9 +10,13 @@ makeDateLabels <- function(dv) {
 		m_end = m_last + 1
 		y_end = y_last
 	}
-
-	lb <- c(u, sprintf("%04d-%02d-01", y_end, m_end))
-	tk <- as.Date(lb, '%Y-%m-%d')
+	lb <- c(u_dv, sprintf("%04d-%02d-01", y_end, m_end))
+	if (posix) {
+		p_lb <- paste(lb, '12:00:00', sep=' ')
+		tk <- as.POSIXct(p_lb)
+	} else {
+		tk <- as.Date(lb, '%Y-%m-%d')
+	}
 	lb <- substr(lb, 1, 7)
 	return(data.frame(tk, lb))
 }
