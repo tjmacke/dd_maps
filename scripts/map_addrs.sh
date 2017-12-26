b5d85dd4267c1cc5c3b7d4432879b709d93b5722
Tom Macke <macke_tom@yahoo.com>
2017-12-26 10:10:25 -0800

    map_addrs.sh	-- simplified the way polygons are added

--- scripts/map_addrs.sh
+++ scripts/map_addrs.sh
@@ -2,7 +2,7 @@
 #
 . ~/etc/funcs.sh
 
-U_MSG="usage: $0 [ -help ] [ -cb_cfg cb-cfg-file ] -at { src | dst } [ address-file ]"
+U_MSG="usage: $0 [ -help ] [ -fl flist-json ] -at { src | dst } [ address-file ]"
 
 if [ -z "$DM_HOME" ] ; then
 	LOG ERROR "DM_HOME is not defined"
@@ -33,7 +33,7 @@ else
 	exit 1
 fi
 
-CB_CFG=
+FLIST=
 ATYPE=
 FILE=
 
@@ -43,14 +43,14 @@ while [ $# -gt 0 ] ; do
 		echo "$U_MSG"
 		exit 0
 		;;
-	-cb_cfg)
+	-fl)
 		shift
 		if [ $# -eq 0 ] ; then
-			LOG ERROR "-cb_cfg requires cf-cfg-file argument"
+			LOG ERROR "-fl requires flist-json argument"
 			echo "$U_MSG" 1>&2
 			exit 1
 		fi
-		CB_CFG="$1"
+		FLIST="$1"
 		shift
 		;;
 	-at)
@@ -94,116 +94,8 @@ fi
 
 rval=0
 
-if [ ! -z "$CB_CFG" ] ; then
-	if [ -z "$CB_HOME" ] ; then
-		LOG ERROR "CB_HOME must be defined to use cartographic boundary information"
-		exit 1
-	fi
-	CB_DATA=$CB_HOME/data
-	CB_BIN=$CB_HOME/bin
-	if [ -z "$FM_HOME" ] ; then
-		LOG ERROR "FM_HOME must be defined to use cartographic boundary information"
-		exit 1
-	fi
-	FM_BIN=$FM_HOME/bin
-	TMP_XFILE=/tmp/cb.sh.$$
-	TMP_JFILE=/tmp/cb.json.$$
-	$AWK '
-	@include '"$CFG_UTILS"'
-	BEGIN {
-		apos = sprintf("%c", 39)
-
-		cb_bin = "'"$CB_BIN"'"
-		cb_data = "'"$CB_DATA"'"
-		fm_bin = "'"$FM_BIN"'"
-
-		tmp_kfile = "/tmp/kml.$$" 
-
-		cb_cfg = "'"$CB_CFG"'"
-		if(CFG_read(cb_cfg, cfg)){
-			err = 1
-			exit err
-		}
-	}
-	END {
-		if(err)
-			exit err
-
-		fmap = cfg["_globals", "fmap"]
-		if(fmap == ""){
-			printf("ERROR: fmap is not set\n") > "/dev/stderr"
-			err = 1
-			exit err
-		}
-		n_fm_ary = split(fmap, fm_ary, "/")
-		if(fm_ary[1] == "$CB_DATA")
-			fm_ary[1] = cb_data
-		x_fmap = fm_ary[1]
-		for(i = 2; i <= n_fm_ary; i++)
-			x_fmap = x_fmap "/" fm_ary[i]
-
-		klist = cfg["_globals", "klist"]
-		if(klist == ""){
-			printf("ERROR: klist is not set\n") > "/dev/stderr"
-			err = 1
-			exit err
-		}
-
-		kfmt = cfg["_globals", "kfmt"]
-		if(kfmt == ""){
-			printf("ERROR: kfmt is not set\n") > "/dev/stderr"
-			err = 1
-			exit err
-		}
-
-		kcolor = cfg["_globals", "kcolor"]
-
-		kpat = cfg["_globals", "kpat"]
-		kfile = cfg["_globals", "kfile"]
-		if(kpat != "" && kfile != ""){
-			printf("ERROR: Use only one of kpat, kfile\n") > "/dev/stderr"
-			err = 1
-			exit err
-		}else if(kpat == "" && kfile == ""){
-			printf("ERROR: Neither kpat or kfile is set\n") > "/dev/stderr"
-			err = 1
-			exit err
-		}
-
-		printf("#! /bin/bash\n")
-		printf("#\n")
-		if(kpat){
-			kf_list = fm_ary[1]
-			for(i = 2; i < n_fm_ary; i++)
-				kf_list = kf_list "/" fm_ary[i]
-			kf_list = kf_list "/*.keys"
-			printf("cat %s |\\\n", kf_list)
-			printf("grep %s%s%s |\\\n", apos, kpat, apos)
-		}else
-			printf("cat %s |\\\n", kfile)
-		printf("%s/getentry -md5 -fmap %s %s > %s\n", fm_bin, x_fmap, kfile, tmp_kfile)
-		printf("%s/kml2geojson -klist \"%s\" -kfmt %s%s%s 2> /dev/null", cb_bin, klist, apos, kfmt, apos)
-		if(kcolor != "")
-			printf(" -kcolor %s", kcolor)
-		printf(" %s\n", tmp_kfile)
-
-		printf("rm -f %s\n", tmp_kfile)
-
-		exit 0
-	}' < /dev/null > $TMP_XFILE
-	rval=$?
-	if [ $rval -ne 0 ] ; then
-		LOG ERROR "polygon extraction script creation failed"
-	elif [ ! -s $TMP_XFILE ] ; then
-		LOG ERROR "empty polygon extraction script"
-		rval=1
-	else
-		chmod +x $TMP_XFILE
-		$TMP_XFILE | $JU_BIN/json_get -g '{geojson}{features}[1:$]' > $TMP_JFILE
-	fi
-fi
-
 awk -F'\t' 'BEGIN {
+	flist = "'"$FLIST"'"
 	atype = "'"$ATYPE"'"
 	f_addr = atype == "src" ? 2 : 3
 
@@ -241,8 +133,12 @@ END {
 		printf("    \"marker-size\": \"small\",\n")
 		printf("    \"marker-color\": \"#aae\"\n")
 		printf("  }\n")
-		printf("}%s\n", p < n_pts ? "," : "")
+		printf("}%s\n", ((p < n_pts) || flist) ? "," : "")
 	}
+	while((getline line < flist) > 0){
+		printf("%s\n", line)
+	}
+	close(flist)
 	pr_trailer()
 	exit 0
 }
