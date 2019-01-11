ccddb109140b38bf3cf6996ef491af6d26f77a80
Tom Macke <macke_tom@yahoo.com>
2019-01-10 16:57:34 -0800

    geocoder_defs.sh	-- renamed GEO_1, GEO_2 to GEO_PRIMARY, GEO_SECONDARY; as GEO_1, GEO_2 will set locally

---
+++ etc/geocoder_defs.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#
+
+# geocoder defs
+GEO_PRIMARY=geo		# geocod.io is the primary geocoder
+GEO_SECONDARY=ocd	# opencagedata.com is the secondary geocoder
+
+function chk_geocoders {
+
+	echo $* |
+	awk 'BEGIN {
+		k_geos["'"$GEO_PRIMARY"'"] = 1
+		k_geos["'"$GEO_SECONDARY"'"] = 1
+	}
+	{
+		n_ary = split($0, ary, ",")
+		for(i = 1; i <= n_ary; i++){
+			sub(/^  */, "", ary[i])
+			sub(/  *$/, "", ary[i])
+			if(!(ary[i] in k_geos)){
+				print "ERROR: unknown geocoder: " ary[i]
+				exit 1
+			}
+			if(!(ary[i] in u_geos)){
+				u_geos[ary[i]] = 1
+				nu_gtab++
+				u_gtab[nu_gtab] = ary[i]
+			}
+		}
+		g_list = ""
+		for(i = 1; i <= nu_gtab; i++)
+			g_list = g_list (i > 1 ? "," : "") u_gtab[i]
+		print g_list
+	}'
+}
